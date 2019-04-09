f2d1a53fd536172236c2fa1760e99e04753e9e01
Soichi Sumi <soichi.sumi@gmail.com>
2019-04-09 15:27:59 +0900

    fix build errors

--- app/server.go
+++ app/server.go
@@ -3,11 +3,20 @@ package app
 import (
 	"context"
 	"github.com/soichisumi/customErrResponse/app/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+func NewServer() Server {return Server{}}
 
 type Server struct{}
 
-func (Server) GetData(context.Context, *proto.GetDataRequest) (*proto.GetDataResponse, error) {
-	return
+func (s Server) GetData(ctx context.Context, req *proto.GetDataRequest) (*proto.GetDataResponse, error) {
+	if !req.Success {
+		return &proto.GetDataResponse{}, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	return &proto.GetDataResponse{
+		Str: "result",
+		Num: 5,
+	}, nil
 }
-
