5c9dc39196501e17e47fc2a74996774050e3a0f0
Soichi Sumi <soichi.sumi@gmail.com>
2019-04-09 15:27:48 +0900

    implement main files

---
+++ cmd/api/api.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/soichisumi/customErrResponse/app"
+	"github.com/soichisumi/customErrResponse/app/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+const (
+	grpcPort = ":3000"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", grpcPort)
+	if err != nil {
+		log.Fatalf("failed to listen: %+v\n", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	server := app.NewServer()
+	proto.RegisterServerServer(grpcServer, server)
+	reflection.Register(grpcServer)
+	fmt.Printf("api is running on port: %s\n", grpcPort)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ cmd/gw/gw.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/GincoInc/forseti/internal/pkg/proto"
+	"github.com/gorilla/handlers"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+	"net/http"
+)
+
+const grpcEndpoint = ":3000"
+const httpEndpoint = ":8080"
+
+func newGateway(ctx context.Context, opts ...runtime.ServeMuxOption) (http.Handler, error) {
+	mux := runtime.NewServeMux(opts...)
+	dialOpts := []grpc.DialOption{grpc.WithInsecure()}
+	conn, err := grpc.Dial(grpcEndpoint, dialOpts...) // grpcサーバのエンドポイント
+	if err != nil {
+		return nil, err
+	}
+
+	err = proto.RegisterForsetiHandler(ctx, mux, conn)
+	if err != nil {
+		return nil, err
+	}
+	err = proto.RegisterHealthCheckServiceHandler(ctx, mux, conn)
+	if err != nil {
+		return nil, err
+	}
+
+	corsMux := handlers.CORS(
+		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "HEAD", "PUT", "DELETE"}),
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedHeaders([]string{"X-Requested-With", "content-type", "authorization"}),
+	)(mux)
+
+	return corsMux, nil
+}
+
+func Run(address string, opts ...runtime.ServeMuxOption) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	gw, err := newGateway(ctx, opts...)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(address, gw)
+}
+
+func main() {
+	fmt.Printf("http api is running on port: %s...\n", httpEndpoint)
+	if err := Run(httpEndpoint); err != nil {
+		panic(err)
+	}
+}
